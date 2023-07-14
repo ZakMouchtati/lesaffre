0e148ee184905e6c7dce189698348594c085f9b9
Zak MOUCHTATI <zakaria.mouchtati01@gmail.com>
2023-07-14 14:11:02 +0100

    Update the landing page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,30 +1,39 @@
-import logo from "../assets/logo-lesaffre.png"
+import logo from "../assets/rafiaa/logo.png"
 import Catalogue from "../components/Catalogue"
 import Form from "../components/Form"
-import footer from "../assets/footer.png"
-
+import banner from "../assets/rafiaa/bande.png"
+import products from "../assets/rafiaa/offre.png"
 const Home = () => {
   return (
     <div className="home">
+        {/* <img src={banner} /> */}
         <div className="flex items-center">
             <div className="container mx-auto">
                 <div className="logo flex justify-center py-6">
-                    <img src={logo} alt="les affre" className="!h-24" />
-                </div>
+                   <img src={logo} alt="les affre" className="!h-24" />
+               </div>
+               <div className="logo flex justify-center py-6">
+                   <img src={banner} alt="les affre" className="!h-24" />
+               </div>
+                {/*
                 <div className="title my-12">
                     <h1 className="text-4xl font-bold text-main text-center uppercase">
-                        Bienvenue Chez le Lesaffre
+                        Bienvenue Chez Lesaffre
                     </h1>
-                </div>
-                <div className="my-20 ">
+                </div> */}
+                <div className="mb-12">
                     <div className=" flex gap-5 justify-center">
                        <Catalogue />
                         <Form />
                     </div>
                 </div>
-                <footer className="flex justify-center">
+                <div className="logo flex justify-center py-6">
+                   <img src={products} alt="les affre" />
+               </div>
+                
+                {/* <footer className="flex justify-center">
                     <img src={footer} alt="" className="img-footer"/>
-                </footer>
+                </footer> */}
             </div>
         </div>
     </div>
